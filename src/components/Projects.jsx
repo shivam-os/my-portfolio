ba5653d82451a0fafafb416818451500188e634e
Shivam <shivam89raj45@gmail.com>
2023-05-25 12:05:50 +0530

    Add projects and fix font size

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,37 @@
+import profilerProject from "../assets/profiler-project.jpeg";
+import nodecmsProject from "../assets/nodecmsProject.jpeg";
+import songifyProject from "../assets/songifyProject.jpeg";
+
 const projectData = [
   {
-    type: "Frontend Project",
-    img: "https://images5.alphacoders.com/810/thumbbig-810547.webp",
+    type: "Full Stack Project",
+    img: profilerProject,
     title: "Profiler",
     content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, ipsum. Amet consequuntur, ab non aliquam ea voluptate labore earum ut omnis aspernatur alias porro perferendis, quaerat hic similique explicabo soluta.",
+      "A web app which allows users to manage all the online personalities they follow in one place. Users can add profiles and all the associated links. MongoDB is used as the database and JWT authentication is implemented with the help of passport. React Hook Form is used for getting the user inputs. Validators are added on both frontend and backend to get proper data from users.",
     github: "https://github.com",
     live: "https://live.com",
-    packages: ["React", "Chakra UI", "Express", "MongoDB", "Passport"],
+    packages: ["React", "Chakra UI", "Express", "MongoDB", "React-Hook-Form", "Yup"],
   },
   {
-    type: "Full Stack Project",
-    img: "https://images5.alphacoders.com/810/thumbbig-810547.webp",
-    title: "Profiler",
+    type: "Backend Project",
+    img: nodecmsProject,
+    title: "NodeBlog API",
     content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, ipsum. Amet consequuntur, ab non aliquam ea voluptate labore earum ut omnis aspernatur alias porro perferendis, quaerat hic similique explicabo soluta.",
-    packages: ["React", "Chakra UI", "Express", "MongoDB", "Passport"],
+      "This API acts as a content management systsm (CMS) for a blog. To handle the blog mangement, there are 3 types of users- Author, Editor & Admin. The admin can add new users and assign role to them. Public endpoints are also provided to access the blog posts. Documentation for this API is done using Postman (It's screenshot can be seen here). ",
+    github: "https://github.com/shivam-os/node-cms",
+    live: "https://bit.ly/node-cms-api",
+    packages: ["Express", "MySQL", "Passport", "Postman", "JWT", "Express-validator"],
+  },
+  {
+    type: "Backend Project",
+    img: songifyProject,
+    title: "Songify API",
+    content:
+      "A spotify like API which allows users to creatae and manage playlists by adding songs to them. The songs are provided via an external unofficial JIO Saavn API. Axios is used to handle the communication with the external API. A trending list of songs is provided to choose from but users also have option to search songs by their name. Documentation for this API is done using Swagger (It's screenshot can be seen here).",
+    github: "https://github.com/shivam-os/songify",
+    live: "https://songify-8fvl.onrender.com/api/docs/",
+    packages: ["Express", "MySQL", "Passport", "Axios", "Swagger", "Express-validator"],
   },
 ];
 
