c2079f7304ab5c8d42cc3982379b1b9ab02d08e7
Shivam <shivam89raj45@gmail.com>
2023-07-17 09:05:55 +0530

    Add updated resume link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
             <a className="nav-link" href="#contact">
               04.Contact
             </a>
-            <a className="nav-link resume" href="https://bit.ly/shivamz-resume" target="_blank" rel="noreferrer">
+            <a className="nav-link resume" href="https://bit.ly/shivamz-resumev2" target="_blank" rel="noreferrer">
               Resume
             </a>
           </div>
